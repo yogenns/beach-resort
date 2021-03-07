13b3654df1fec4866830d1bb6244cd2037c766a3
yogenns <yogenns@gmail.com>
2021-03-07 21:25:28 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Error from './pages/Error'
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import { BrowserRouter as Router, Router, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/rooms" component={Rooms} exact />
+        <Route path="/rooms/:slug" component={SingleRoom} exact />
+        <Route component={Error} />
+      </Switch>
+    </Router>
   );
 }
 
